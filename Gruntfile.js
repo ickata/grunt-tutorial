ce444b670437fec876b2537b62f907726d2bade9
Hristo Chakarov <ickata@HristoMBA-2.local>
2014-06-18 22:08:25 +0300

    Grunt plugin: Sass + test files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,41 @@
 module.exports = function ( grunt ) {
+   // globals
+   var sass_files = [{
+      expand   : true,
+      cwd      : 'project/sass/',      // current working directory
+      dest     : 'project/css/',
+      src      : '**/*.{sass, scss}',
+      ext      : '.css'
+   }];
    // load NPM tasks
    [
+      'grunt-contrib-sass',
       'grunt-contrib-jshint'
    ].forEach( function ( name ) {
       this.loadNpmTasks( name );
    }, grunt );
    // init configuration
    grunt.initConfig({
+      sass     : {
+         options  : {
+            cacheLocation  : '.tmp/.sass-cache'
+         },
+         // sub-task
+         dev      : {
+            options  : {
+               style          : 'expanded',
+               lineComments   : true
+            },
+            files    : sass_files
+         },
+         // sub-task
+         prod     : {
+            options  : {
+               style          : 'compressed'
+            },
+            files    : sass_files
+         }
+      },
       jshint   : {
          all      : [
             'Gruntfile.js',
